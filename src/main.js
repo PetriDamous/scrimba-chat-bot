71ce43514b73d9136a1de02b5a2109f39dca6e70
Petri <dimtri.williams.6448@gmail.com>
2025-03-20 19:19:06 -0500

    Setup first chain for standalone question using tiiuae/falcon-7b-instruc model.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,37 @@
+import { HuggingFaceInference } from "@langchain/community/llms/hf";
+import { PromptTemplate } from "@langchain/core/prompts";
+
+const hfApiKey = import.meta.env.VITE_HUGGINGFACE_API_KEY;
+
+const hfTextGen = new HuggingFaceInference({
+  model: "tiiuae/falcon-7b-instruct",
+  temperature: 0.7,
+  maxTokens: 200,
+  apiKey: hfApiKey,
+});
+
+const standaloneQuestionTemplate =
+  "Given a question, convert it to a standalone question. question: {question} standalone question:";
+
+const standalonePrompt = PromptTemplate.fromTemplate(
+  standaloneQuestionTemplate
+);
+
+const standaloneQuestionChain = standalonePrompt.pipe(hfTextGen);
+
+const result = await standaloneQuestionChain.invoke({
+  question:
+    "What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful. I really like playing Halo so maybe I can upgrade my latop to play Scrimba? I love turtles btw how do you feel about turtles?",
+});
+
+console.log(result);
+
 document.addEventListener("submit", (e) => {
   e.preventDefault();
   progressConversation();
 });
 
-// const openAIApiKey = process.env.OPENAI_API_KEY;
-
-async function progressConversation() {
+const response = await async function progressConversation() {
   const userInput = document.getElementById("user-input");
   const chatbotConversation = document.getElementById(
     "chatbot-conversation-container"
@@ -26,4 +52,4 @@ async function progressConversation() {
   chatbotConversation.appendChild(newAiSpeechBubble);
   newAiSpeechBubble.textContent = result;
   chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
-}
+};
